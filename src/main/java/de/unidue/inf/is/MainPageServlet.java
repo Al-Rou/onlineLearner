4b9668e58ba75f1fa6342724ebc71733cac4c0c1
Al-Rou <roal.24.alro@gmail.com>
2021-02-01 23:31:51 -0500

    MainPage underway1

--- src/main/java/de/unidue/inf/is/MainPageServlet.java
+++ src/main/java/de/unidue/inf/is/MainPageServlet.java
@@ -1,6 +1,15 @@
 package de.unidue.inf.is;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MainPageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        request.getAttribute("mycourse");
+    }
 }
