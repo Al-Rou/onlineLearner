807d2dae8514c2dc61b8fa140308f7738cd2c79f
Al-Rou <roal.24.alro@gmail.com>
2021-02-04 21:44:22 -0500

    New course underway2

--- src/main/java/de/unidue/inf/is/CreateCourseServlet.java
+++ src/main/java/de/unidue/inf/is/CreateCourseServlet.java
@@ -1,6 +1,20 @@
 package de.unidue.inf.is;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class CreateCourseServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private String errorMessage = "";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException
+    {
+        request.setAttribute("error", errorMessage);
+        request.getRequestDispatcher("/newCoursePage.ftl").forward(request, response);
+    }
 }
