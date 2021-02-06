da1426ecd70cf553aaef0518ffdc404631e8d6d4
Al-Rou <roal.24.alro@gmail.com>
2021-02-05 20:09:31 -0500

    New course underway6

--- src/main/java/de/unidue/inf/is/CreateCourseServlet.java
+++ src/main/java/de/unidue/inf/is/CreateCourseServlet.java
@@ -17,4 +17,21 @@ public class CreateCourseServlet extends HttpServlet {
         request.setAttribute("error", errorMessage);
         request.getRequestDispatcher("/newCoursePage.ftl").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException
+    {
+        String newName = request.getParameter("titel");
+        String newPass = request.getParameter("pass");
+        String newFreeSeats = request.getParameter("seats");
+        int newFreeSeatsInt = Integer.parseInt(newFreeSeats);
+        String newDescrip = request.getParameter("descrip");
+        if ((newName.length()==0) || (newName.length()>50))
+        {
+            errorMessage = "";
+            errorMessage += "The entered name is either longer than 50 characters or empty";
+            doGet(request, response);
+        }
+    }
 }
