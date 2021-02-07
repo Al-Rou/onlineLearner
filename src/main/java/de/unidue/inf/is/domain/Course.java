32bebb3e378e079711b299fa12b488822390da63
Al-Rou <roal.24.alro@gmail.com>
2021-02-07 02:37:08 -0500

    Course Details underway6

--- src/main/java/de/unidue/inf/is/domain/Course.java
+++ src/main/java/de/unidue/inf/is/domain/Course.java
@@ -15,6 +15,15 @@ public class Course {
         this.freiePlaetze = freiePlaetze;
         this.ersteller = ersteller;
     }
+
+    public Course(String name, String beschreibungsText, String einschreibeSchluessel, int freiePlaetze, int ersteller) {
+        this.name = name;
+        this.beschreibungsText = beschreibungsText;
+        this.einschreibeSchluessel = einschreibeSchluessel;
+        this.freiePlaetze = freiePlaetze;
+        this.ersteller = ersteller;
+    }
+
     public Course(int kID, String name, int ersteller, int freiePlaetze) {
         this.name = name;
         this.freiePlaetze = freiePlaetze;
