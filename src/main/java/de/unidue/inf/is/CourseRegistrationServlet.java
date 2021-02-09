d39cfcef7daf397c27142835d0bd874e2cc778d2
Al-Rou <roal.24.alro@gmail.com>
2021-02-08 21:59:24 -0500

    Course Registration underway2

--- src/main/java/de/unidue/inf/is/CourseRegistrationServlet.java
+++ src/main/java/de/unidue/inf/is/CourseRegistrationServlet.java
@@ -1,6 +1,20 @@
 package de.unidue.inf.is;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class CourseRegistrationServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static String errorMessage = "";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        request.setAttribute("error", errorMessage);
+        request.getRequestDispatcher("/registerPage.ftl").forward(request, response);
+    }
 }
