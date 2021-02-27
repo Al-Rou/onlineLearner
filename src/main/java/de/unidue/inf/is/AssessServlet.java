4eb9e63f99e268dec8f862122966b5a99c253e05
Al-Rou <roal.24.alro@gmail.com>
2021-02-26 22:56:43 -0500

    Assess an answer underway4

--- src/main/java/de/unidue/inf/is/AssessServlet.java
+++ src/main/java/de/unidue/inf/is/AssessServlet.java
@@ -1,7 +1,26 @@
 package de.unidue.inf.is;
 
+import de.unidue.inf.is.domain.TaskToShow;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AssessServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static String errorMessage = "";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException
+    {
+        List<TaskToShow> emptyList = new ArrayList<>();
+        request.setAttribute("registered", emptyList);
+        request.setAttribute("error", errorMessage);
+        request.getRequestDispatcher("/assessPage.ftl").forward(request, response);
+    }
 }
