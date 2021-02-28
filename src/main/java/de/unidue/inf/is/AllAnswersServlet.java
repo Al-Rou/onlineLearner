345dd390a7397cfca8189d47ffa5442f8335d4dd
Al-Rou <roal.24.alro@gmail.com>
2021-02-27 20:33:04 -0500

    Assess an answer underway18

--- src/main/java/de/unidue/inf/is/AllAnswersServlet.java
+++ src/main/java/de/unidue/inf/is/AllAnswersServlet.java
@@ -1,4 +1,31 @@
 package de.unidue.inf.is;
 
-public class AllAnswersServlet {
+import de.unidue.inf.is.domain.Delivery;
+import de.unidue.inf.is.domain.Task;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllAnswersServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static String errorMessage = "";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        List<Task> list = new ArrayList<>();
+        list.add(new Task(7, 62, "SQL", "What is SQL?"));
+        List<Delivery> list2 = new ArrayList<>();
+        list2.add(new Delivery(5, "I don't know!"));
+        request.setAttribute("error", errorMessage);
+        request.setAttribute("aufgabe", list);
+        request.setAttribute("abgabe", list2);
+        request.getRequestDispatcher("/allAnswersPage.ftl").forward(request,response);
+    }
 }
