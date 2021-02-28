e43f1324e22d26798ed1aaf48ab71c288782dcc3
Al-Rou <roal.24.alro@gmail.com>
2021-02-27 19:15:43 -0500

    Assess an answer underway13

--- src/main/java/de/unidue/inf/is/AllTasksServlet.java
+++ src/main/java/de/unidue/inf/is/AllTasksServlet.java
@@ -1,6 +1,27 @@
 package de.unidue.inf.is;
 
+import de.unidue.inf.is.domain.TaskToShow;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllTasksServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static String errorMessage = "";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        List<TaskToShow> emptyList = new ArrayList<>();
+        //emptyList.add(new TaskToShow("SQL",2,"What","Why is SQL needed?",6));
+        request.setAttribute("error", errorMessage);
+        request.setAttribute("kurse", emptyList);
+        request.getRequestDispatcher("/allTasksPage.ftl").forward(request,response);
+    }
 }
